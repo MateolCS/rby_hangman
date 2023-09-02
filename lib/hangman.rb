953fa1f1db7bbabaa94a80d6ab0c8525902e1787
MateoLCS <mateusz.namysl0318@gmail.com>
2023-09-02 23:51:05 +0200

    hangman: game logic implementation

--- lib/hangman.rb
+++ lib/hangman.rb
@@ -1,17 +1,45 @@
 class Hangman
   def initialize
     @word = get_word
-    @guessed_letters = Array.new(@word.length, "_")
+    # @guessed_letters = Array.new(@word.length, "_")
     @wrong_letters = []
-    @guesses_left = @word.length + 4
+    # @guesses_left = @word.length + 4
+    puts @word
   end
 
   def get_word
-    words = File.readlines("./lib/5desk.txt")
+    words = File.readlines("words.txt", 'r')
+    puts words
     words.select! { |word| word.length.between?(5, 12) }
     words.sample.downcase.chomp
   end 
 
+  def play_round(in_letter)
+    if @word.include?(in_letter)
+      @word.split("").each_with_index do |letter, index|
+        @guessed_letters[index] = letter if letter == in_letter
+      end
+    else
+      @wrong_letters << in_letter
+      @guesses_left -= 1
+    end
+
+    display_game_status
+  end
+
+  def game_over?
+    if @guesses_left == 0 || @guessed_letters.join == @word
+      true
+    else
+      false
+    end
+  end
+
+  def display_game_status
+    puts "Guesses left: #{@guesses_left}"
+    puts "Wrong letters: #{@wrong_letters}"
+    puts "Word: #{@guessed_letters}"
+  end
 
 
 end
