b4d4e56a7aee3b2321496cda42cd86838b998ae8
MateoLCS <mateusz.namysl0318@gmail.com>
2023-09-13 22:20:02 +0200

    hangman: fixed get_word bug, word is now fetched correctly, finished class implementation

--- lib/hangman.rb
+++ lib/hangman.rb
@@ -1,18 +1,24 @@
+#Class that represents the game of hangman and is saved and loaded by Game class
+# get_word - gets random word from words.txt file
+# play_round - takes letter as input and checks if it is in the word and displays game status
+# game_over? - checks if game is over
+# display_game_status - displays game status
+
+
 class Hangman
   def initialize
     @word = get_word
-    # @guessed_letters = Array.new(@word.length, "_")
+    @guessed_letters = Array.new(@word.length, "_")
     @wrong_letters = []
-    # @guesses_left = @word.length + 4
-    puts @word
+    @guesses_left = @word.length + 4
   end
 
   def get_word
-    words = File.readlines("words.txt", 'r')
-    puts words
+    file = File.open(Dir.pwd + "/data/words.txt")
+    words = file.readlines.map(&:chomp)
     words.select! { |word| word.length.between?(5, 12) }
     words.sample.downcase.chomp
-  end 
+  end
 
   def play_round(in_letter)
     if @word.include?(in_letter)
@@ -40,6 +46,7 @@ class Hangman
     puts "Wrong letters: #{@wrong_letters}"
     puts "Word: #{@guessed_letters}"
   end
-
-
 end
+
+
+
